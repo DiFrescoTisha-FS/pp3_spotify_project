0effde354717272015d02a0f921085ddfbf0d7ff
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2023-05-08 10:42:06 -0400

    updated ui

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar";
-import { Cursor, useTypewriter } from "react-simple-typewriter"
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 export default function SearchForm({
   searchTerm,
@@ -15,26 +15,36 @@ export default function SearchForm({
     delaySpeed: 5000,
   });
 
+  let resultTitle;
+
+  if (searchType === "artist") {
+    resultTitle = "Artist";
+  } else if (searchType === "album") {
+    resultTitle = "Album";
+  } else if (searchType === "playlist") {
+    resultTitle = "Playlist";
+  }
+
   return (
     <div className="">
       <Navbar />
       <div className="h-screen flex flex-col mx-20 items-center justify-center text-center overflow-hidden text-white">
         <div className="z-20">
           <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-            Artist Album Playlist
+            Album *  Artist * Playlist
           </h2>
           <h1 className="text-5xl lg:text-6xl font-semibold px-10">
             <span className="mr-3">{text}</span>
             <Cursor cursorColor="#f7abba" />
           </h1>
         </div>
-
+  
         <div className="w-full flex justify-evenly mt-16 items-center">
           <form
             onSubmit={onSearchSubmit}
             className="form text-black flex flex-col md:flex-row md:space-x-4 m-10 mt-6 p-5 mx-auto w-[100%] p-[75px 5px 5px 5px] bg-black"
           >
-            <label htmlFor="search-term" className="sr-only">
+            <label htmlFor="search-term" className="sr-only text-">
               Search:
             </label>
             <input
@@ -43,28 +53,48 @@ export default function SearchForm({
               value={searchTerm}
               onChange={onSearchTermChange}
               placeholder="Enter search term"
-              className="input text-white lg:w-1/3 justify-evenly mr-12" 
+              className="input text-white lg:w-1/3 justify-evenly mr-12"
             />
-            <label htmlFor="search-type" className="sr-only">
-              Search for:
-            </label>
-            <select
-              id="search-type"
-              value={searchType}
-              onChange={onSearchTypeChange}
-              className="option_button block w-full lg:w-1/3 p-2 rounded-md shadow-sm"
-            >
-              <option value="artist">Artists</option>
-              <option value="album">Albums</option>
-              <option value="playlist">Playlists</option>
-            </select>
+<select 
+  defaultValue={null}
+  id="search-type"
+  onChange={onSearchTypeChange}
+  className="option_button block w-full lg:w-1/3 p-2 rounded-md shadow-sm"
+>
+  <option value="">Select a Category</option>
+  <option value="artist">Artist</option>
+  <option value="album">Album</option>
+  <option value="playlist">Playlist</option>
+</select>
+
             <button type="submit" className="btn lg:w-1/4">
               Search
             </button>
           </form>
         </div>
+        {searchTerm && (!searchType || searchType === '') ? (
+  <h2 className="text-sm text-white">
+    Please select a category
+  </h2>
+) : searchTerm && searchType && searchResults && searchResults.length > 0 ? (
+  <>
+    <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+      {searchType.charAt(0).toUpperCase() + searchType.slice(1)}:{" "}
+      {searchTerm}: {resultTitle}
+    </h2>
+    <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+  `${resultTitle}: ${searchTerm}`
+</h2>
+
+    {/* Display the search results here */}
+  </>
+) : (
+  <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+            {/* {resultTitle || "Search"} */}
+          </h2>
+)}
+
+        <hr className="line" />
       </div>
     </div>
-  );
-}
-
+  )};
--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,23 +1,31 @@
 export default function SearchResult({ searchResults }) {
   return (
-    <div className="container-md flex flex-wrap justify-evenly mx-10 gap-5">
-      {searchResults.map((result) => (
-        <div key={result.id} className="card flex flex-col items-center mb-4 px-2 w-full md:w-1/3 lg:w-1/6">
-          {result.images && result.images.length > 0 && (
-            <img src={result.images[0].url} alt={result.name} className="w-full h-38 object-cover rounded-t-lg" />
-          )}
-          <div className="p-4">
-            <h2 className="text-lg font-semibold">{result.name}</h2>
-            {result.type === "artist" ? (
-              <p className="text-gray-500">Artist</p>
-            ) : result.type === "album" ? (
-              <p className="text-gray-500">Album</p>
-            ) : (
-              <p className="text-gray-500">Playlist</p>
+    <div className="container-md flex flex-wrap justify-center mx-10 gap-6">
+      {searchResults.length === 0 ? null : (
+        searchResults.map((result) => (
+          <div key={result.id} className="card flex flex-col text-xs items-center h-48 max-w-[200px] border border-gray-300 rounded-lg">
+            {result.images && result.images.length > 0 && (
+              <img src={result.images[0].url} alt={result.name} className="h-32 w-32 object-cover rounded-t-lg" />
             )}
+            <div className="p-2">
+              {result.type === "artist" ? (
+                <h3 className="text-sm font-semibold">{result.name}</h3>
+              ) : result.type === "album" ? (
+                <h3 className="text-sm font-semibold">{result.name}</h3>
+              ) : (
+                <h3 className="text-sm font-semibold">{result.name}</h3>
+              )}
+              {result.type === "artist" ? (
+                <p className="text-gray-500">Artist</p>
+              ) : result.type === "album" ? (
+                <p className="text-gray-500">Album</p>
+              ) : (
+                <p className="text-gray-500">Playlist</p>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
